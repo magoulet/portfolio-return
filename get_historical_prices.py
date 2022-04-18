a6dcc8fee33db3aa97f838bf1f13c70377ffa79d
magoulet <magoulet@gmail.com>
2022-04-18 14:28:09 -0700

    added feature to get prices from pickles

---
+++ get_historical_prices.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime as dt
+import json
+import mysql.connector
+import numpy as np
+import operator
+import os
+import pandas as pd
+import pickle
+import psutil
+import requests
+import time
+import yfinance as yf
+from portfolio_classes import Security
+from Portfolio_Return_oo import argParser, getconfig, readTransactions, getPrice
+
+
+def dumpHistoricalPrice(portfolio, path):
+    for k, v in portfolio.items():
+        if os.path.exists(path+v.ticker+'.pickle'):
+            with open(path+v.ticker+'.pickle', 'rb') as file:
+                df = pickle.load(file)
+                try:
+                    lastDate = df.index[-1]
+                    if (lastDate < pd.Timestamp(v.lastTradeDate)) or (v.lastTradeDate is None):
+                        df = pd.concat([df, yf.download(k, start=lastDate, end=v.lastTradeDate)])
+                except Exception:
+                    print('Error getting date for: {}, lastDate: {}'.format(v.ticker, lastDate))
+        else:
+            df = yf.download(k, start=v.firstTradeDate, end=v.lastTradeDate)
+        with open(path+v.ticker+'.pickle', 'wb') as file:
+            pickle.dump(df, file)
+
+
+def checkPickle(ticker, path):
+    try:
+        with open(path+ticker+'.pickle', 'rb') as file:
+            df = pickle.load(file)
+            print(df.head())
+    except Exception:
+        print('Could not find pickle')
+
+    return None
+
+
+def createDir(path):
+    # Check whether the specified path exists or not
+    isExist = os.path.exists(path)
+
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(path)
+    return None
+
+
+if __name__ == "__main__":
+
+    config = getconfig()
+    dataTable = {'trades': config["mysql"][0]["TransactionTable"]}
+    path = config["directories"][0]["pickles"]
+    currencies = eval(config['misc'][0]["Currencies"])
+
+    args = argParser()
+
+    if args.date:
+        try:
+            date = dt.datetime.strptime(args.date, "%Y-%m-%d").date()
+        except ValueError:
+            msg = "Not a valid date: '{0}'.".format(args.date)
+            raise argparse.ArgumentTypeError(msg)
+    else:
+        date = dt.date.today()
+
+    portfolio = {}
+
+    createDir(path)
+
+    for currency in currencies:
+        portfolio[currency] = readTransactions(dataTable['trades'], date,
+                                               currency, config)
+        dumpHistoricalPrice(portfolio[currency], path)
--- portfolio_classes.py
+++ portfolio_classes.py
@@ -10,10 +10,13 @@ class Security:
         self.costBasis = 0
         self.logReturn = 0
         self.UnrealizedReturn = 0
-        self.totUnrealizedReturn= 0
+        self.totUnrealizedReturn = 0
+        self.firstTradeDate = None
+        self.lastTradeDate = None
 
 
-    def buy(self, qty, price):
+
+    def buy(self, qty, price, date):
 
         if qty > 0:
             self.qty += qty
@@ -22,15 +25,25 @@ class Security:
         self.moneyIn += qty*price
         self.costBasis = self.moneyIn / self.qty
 
+        # A position was closed in the past, then opened again
+        if self.firstTradeDate is None:
+            self.firstTradeDate = date
+        if self.lastTradeDate is not None:
+            self.lastTradeDate = None
+
         # print("Bought {} shares of {} at ${:,} for a total amount of ${:,}".format(qty, self.ticker, price, qty*price))
 
 
-    def sell(self, qty, price, commission):
+    def sell(self, qty, price, commission, date):
         # cost basis is constant for a sale transaction
         # Output: realGain, moneyIn
 
         if qty > 0:
             self.qty -= qty
+
+            # Record date when position was closed
+            if self.qty == 0:
+                self.lastTradeDate = date
         else:
             print('Cannot sell a negative quantity!')
         self.realGain += qty * (price - self.costBasis)
@@ -40,6 +53,8 @@ class Security:
         # print("Sold {} shares of {} at ${:,} for a total amount of ${:,}".format(qty, self.ticker, price, qty*price))
 
     def print(self):
-        print('Ticker: {} \nQty: {} \nPrice: {:,} \nMoney In: {:,.0f} \nrealGain: {:,.0f} \nCost Basis: {:,.2f} \nReturn: {:.2f}%'.format(self.ticker, self.qty, self.price, self.moneyIn, self.realGain, self.costBasis, (math.e**self.logReturn-1)*100))
+        print('Ticker: {} \nQty: {} \nPrice: {:,} \nMoney In: {:,.0f} \nrealGain: {:,.0f} \nCost Basis: {:,.2f} \nReturn: {:.2f}% \nFirst Trade Date: {} \nLast Trade Date: {}'.format(self.ticker, self.qty, self.price, self.moneyIn, self.realGain, self.costBasis, (math.e**self.logReturn-1)*100, self.firstTradeDate, self.lastTradeDate))
 
-    # def split(self, ratio):
+    def split(self, ratio):
+        self.qty *= ratio
+        self.price *= 1/ratio
