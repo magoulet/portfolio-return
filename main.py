b27f7496acb84e104613d718937ad065a11d0357
M-A Goulet <magoulet@gmail.com>
2023-06-24 21:09:01 -0700

    Define configuration and argument globally; some more cleanup

--- main.py
+++ main.py
@@ -7,42 +7,38 @@ import datetime as dt
 import json
 import os
 import pandas as pd
+import pathlib
 import pickle
 import psutil
 import requests
 import time
 import yfinance as yf
 
-def getconfig():
-    path = os.path.dirname(os.path.abspath(__file__))
-    configuration_file = path + '/config.json'
-    config = json.loads(open(configuration_file).read())
 
-    return config
+# Read configuration
+configuration_file = pathlib.Path('config.json')
+config = json.loads(open(configuration_file).read())
 
-
-def arg_parser():
-    parser = argparse.ArgumentParser(description='Program Flags')
-    parser.add_argument('-d', help='Snapshot date - format YYYY-MM-DD',
-                        action="store", dest='date', default=False)
-    parser.add_argument('-m', help='Sends notification message to user',
-                        action="store_true", dest='sendmail', default=False)
-    args = parser.parse_args()
-
-    return args
+# Parse arguments
+parser = argparse.ArgumentParser(description='Program Flags')
+parser.add_argument('-d', help='Snapshot date - format YYYY-MM-DD',
+                    action="store", dest='date', default=False)
+parser.add_argument('-m', help='Sends notification message to user',
+                    action="store_true", dest='sendmail', default=False)
+args = parser.parse_args()
 
 
-def read_contributions(table, date, currency, config):
+def read_contributions(table, date, currency):
     cashflows = Cashflows(currency)
-    df = read_db_contributions(config, table, date, currency)
+    df = read_db_contributions(table, date, currency)
     for index, row in df.iterrows():
         cashflows.event(row.date, row.contribution)
 
     return cashflows
 
 
-def build_portfolio(table, date, currency, config):
-    df = read_db_transactions(config, table, date, currency)
+def build_portfolio(table, date, currency):
+    df = read_db_transactions(table, date, currency)
 
     # Adjust for stock splits
     df['Units'] = df['Units'] * df['SplitRatio']
@@ -94,11 +90,11 @@ def get_price(tickers, date, path, offline=None):
 
 
 
-def database_helper(sql_command, sql_operation, config):
-    host = config["mysql"][0]["host"]
-    user = config["mysql"][0]["user"]
-    password = config["mysql"][0]["password"]
-    database = config["mysql"][0]["database"]
+def database_helper(sql_command, sql_operation):
+    host = config["mysql"]["host"]
+    user = config["mysql"]["user"]
+    password = config["mysql"]["password"]
+    database = config["mysql"]["database"]
 
     # Create the SQLAlchemy engine
     engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
@@ -126,43 +122,43 @@ def database_helper(sql_command, sql_operation, config):
     return data
 
 
-def read_db_output(config, table):
+def read_db_output(table):
     sql_command = "SELECT date, value FROM %s;" \
                   % table
-    data = database_helper(sql_command, "Select", config)
+    data = database_helper(sql_command, "Select")
     return data
 
-def write_db_output(end, tot_value, tot_contributions, config, table):
+def write_db_output(end, tot_value, tot_contributions, table):
     sql_command = "INSERT INTO %s VALUES ('%s', '%s', '%s') ON DUPLICATE KEY \
                  UPDATE value=VALUES(value), \
                  contributions=VALUES(contributions);" \
                  % (table, end, tot_value, tot_contributions)
-    database_helper(sql_command, "Insert", config)
+    database_helper(sql_command, "Insert")
     return None
 
 
-def read_db_transactions(config, table, date, currency):
+def read_db_transactions(table, date, currency):
     sql_command = "SELECT * FROM %s WHERE Date <= '%s' \
                   AND LOWER(Currency) = '%s';" \
                   % (table, date, currency)
-    data = database_helper(sql_command, "Select", config)
+    data = database_helper(sql_command, "Select")
     return data
 
 
-def read_db_contributions(config, table, date, currency):
+def read_db_contributions(table, date, currency):
     sql_command = "SELECT * FROM %s WHERE Date <= '%s' \
                  AND LOWER(currency) = '%s';" \
                  % (table, date, currency)
-    data = database_helper(sql_command, "Select", config)
+    data = database_helper(sql_command, "Select")
     return data
 
 
-def read_time_history(config, table, date):
+def read_time_history(table, date):
     sql_command = "(SELECT * FROM %s WHERE date <= '%s' \
                   ORDER BY Date desc LIMIT 2) \
                   ORDER BY Date asc;" \
                   % (table, date)
-    data = database_helper(sql_command, "Select", config)
+    data = database_helper(sql_command, "Select")
     return data
 
 
@@ -192,16 +188,13 @@ def main():
     exec_time = 0
     start_time = time.time()
 
-    config = getconfig()
-    timing = eval(config["misc"][0]["Timing"])
-    data_table = {'contributions': config["mysql"][0]["ContributionTable"],
-                 'trades': config["mysql"][0]["TransactionTable"]}
-    result_table = {'CAD': config["mysql"][0]["ResultTableCAD"],
-                   'USD': config["mysql"][0]["ResultTableUSD"]}
-    path = config["directories"][0]["pickles"]
-    currencies = eval(config['misc'][0]["Currencies"])
-
-    args = arg_parser()
+    timing = eval(config["misc"]["Timing"])
+    data_table = {'contributions': config["mysql"]["ContributionTable"],
+                 'trades': config["mysql"]["TransactionTable"]}
+    result_table = {'CAD': config["mysql"]["ResultTableCAD"],
+                   'USD': config["mysql"]["ResultTableUSD"]}
+    pickle_path = config["directories"]["pickles"]
+    currencies = eval(config['misc']["Currencies"])
 
     if args.date:
         try:
@@ -218,7 +211,7 @@ def main():
         portfolio[currency] = build_portfolio(data_table['trades'],
                                               date,
                                               currency,
-                                              config)
+                                              )
 
         # Get a list of active tickers (non-null qty) in the portfolio
         active_tickers = []
@@ -226,7 +219,7 @@ def main():
             if value.qty > 0:
                 active_tickers.append(value.ticker)
 
-        prices = get_price(active_tickers, date, path)
+        prices = get_price(active_tickers, date, pickle_path)
 
         # Initialize variable that we'll use later
         tot_value = 0
@@ -271,12 +264,12 @@ def main():
 
         # Contributions to date
         cashflows = read_contributions(
-            data_table['contributions'], date, currency, config)
+            data_table['contributions'], date, currency)
         tot_contributions = -1* cashflows.total() # IRR calculations: cash contributed is negative
 
         # Calculate MWRR (XIRR) since inception, not including today
         # Get all cashflows
-        portfolio_values = read_db_output(config, result_table[currency])
+        portfolio_values = read_db_output(result_table[currency])
 
         # Get initial and today's values
         initial_date, initial_value = portfolio_values.iloc[0]
@@ -306,11 +299,11 @@ def main():
                       tot_real_gain, initial_date.strftime("%Y-%m-%d"), mwrr*100))
 
         print("Writing to the DB...")
-        write_db_output(date, round(tot_value, 2), tot_contributions, config,
+        write_db_output(date, round(tot_value, 2), tot_contributions,
                         result_table[currency])
 
         if args.sendmail:
-            time_history = read_time_history(config, result_table[currency],
+            time_history = read_time_history(result_table[currency],
                                             dt.date.today())
             daily_delta = get_daily_variation(time_history)
 
@@ -321,7 +314,7 @@ def main():
             print(body)
             # sendmail(sender, to, subject, body)
             print("Sending mail to user...")
-            telegram_notification(config['telegram'][0], body)
+            telegram_notification(config['telegram'], body)
 
     finish_time = time.time()
     exec_time += finish_time-start_time
